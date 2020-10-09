20d1d15bde900c8b5b4c7d8f8ebc6a5f0462defc
vladPalev <palev.vladislav@gmail.com>
2020-10-09 09:54:09 +0000

    Step 11

---
+++ webapp/ext/controller/CustomView.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("saprun.sber.ext.controller.CustomView", {
+		onInit: function () {
+		
+		},
+		
+		onAfterRendering: function (parameter) {
+			var q = 1;
+		}
+	});
+
+});
